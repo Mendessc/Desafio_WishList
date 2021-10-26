6fc7e975f5a4b4e3fd0b527557664c51f162b6e1
Lucas <lucask.silvamendes@gmail.com>
2021-10-26 16:32:28 -0300

    bd criado

--- BD/Scripts/DML_WishList.sql
+++ BD/Scripts/DML_WishList.sql
@@ -1,11 +1,11 @@
 Use WishList;
 Go
 
-insert into Usuarios(email, senha,)
-values('marcospaulo@email.com', '123456'),('matheus@email.com','456789')
+insert into Usuarios(email, senha)
+values('marcospaulo@email.com', '123456'),('matheus@email.com','456789'),('lucas@email.com','777')
 Go
 
 
-insert into Desejos(descricao)
-values ('òculos',1),('Calça',2),('Tênis',1)
+insert into Desejos(descricao, idUsuaio)
+values ('pular de paraquedas',1),('ser rico e conseguir comprar tudo que quiser',2),('viajar pelo mundo',1),('andar de helicoptero',3)
 Go
